f0c85d35873ba8cb0583a1109c5884ff5ff99b67
keitaromw <keitaro.wongso@gmail.com>
2021-02-21 16:29:15 +0700

    Navigation Drawer without button listener.

---
+++ app/src/main/java/com/bluekaui/Drawer.java
@@ -0,0 +1,18 @@
+package com.bluekaui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Drawer extends AppCompatActivity {
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_layout);
+
+
+    }
+}
