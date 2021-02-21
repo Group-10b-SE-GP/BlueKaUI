e49700ed2312aaf9de3b77ca3250278e823e5c2f
keitaromw <keitaro.wongso@gmail.com>
2021-02-21 17:11:14 +0700

    Navigation Drawer without button listener.

--- app/src/main/java/com/bluekaui/Drawer.java
+++ app/src/main/java/com/bluekaui/Drawer.java
@@ -2,17 +2,55 @@ package com.bluekaui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Drawer extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Drawer extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    Toolbar toolBar;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_layout);
 
+        toolBar = findViewById(R.id.toolbardrawer);
+        setSupportActionBar(toolBar);
 
+        mDrawerLayout = findViewById(R.id.drawerLayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,toolBar,R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+         return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+        super.onBackPressed();
+        }
     }
 }
